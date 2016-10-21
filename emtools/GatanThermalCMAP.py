d64307893f227ef328c020a272523be09bfc165f
AndrewHerzing <aaherzing@gmail.com>
2016-10-21 10:32:46 -0400

    Add files via upload

---
+++ emtools/GatanThermalCMAP.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 08 17:50:09 2016
+
+@author: aherzing
+"""
+import matplotlib
+my_cmap = matplotlib.colors.LinearSegmentedColormap.from_list('my_colormap',['black','blue','green','red','yellow','white'],256,1.0)
---
+++ setup.py
@@ -0,0 +1,2 @@
+from distutils.core import setup
+setup(name='EMTools',version='0.1',py_modules=['EMTools'])
