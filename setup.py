df886b4776a8f0f284c62af385e41a3597c0796c
Andrew Herzing <aaherzing@gmail.com>
2019-04-25 19:36:03 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,16 @@
-from distutils.core import setup
-setup(name='emtools',version='0.1',py_modules=['emtools'])
+"""Setup file for emtools package."""
+
+from setuptools import setup
+
+setup(
+    name='emtools',
+    version='0.1.0',
+    author='Andrew A. Herzing',
+    description='Python tools for processing electron microscopy data ',
+    packages=[
+        'emtools',
+        ],
+    install_requires=[
+        pymcr,
+    ]
+)
