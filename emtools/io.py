41dcdb923d2baee238bf809d65702524779fc8d8
Andrew Herzing <aaherzing@gmail.com>
2019-06-17 19:04:48 +0000

    Removed redundant line in save_axsia

--- emtools/io.py
+++ emtools/io.py
@@ -10,7 +10,6 @@ def save_axsia(s, filename=None):
     savedict['ncols'] = np.float64(s.data.shape[0])
     savedict['nchannels'] = np.float64(s.data.shape[2])
     savedict['resolution'] = np.float64(s.axes_manager[-1].scale)
-    savedict['nrows'] = np.float64(s.data.shape[0])
     s.unfold()
     savedict['specdata'] = s.data.T
 
