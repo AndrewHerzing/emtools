586cfbd3070c36066f7513e8e3db34b776f225d8
Herzing <aherzing@NIST.GOV>
2019-06-19 09:39:23 -0400

    Fixed flake8 errors

--- emtools/io.py
+++ emtools/io.py
@@ -1,5 +1,6 @@
 from scipy.io import savemat, loadmat
 import numpy as np
+import hyperspy.api as hs
 
 
 def save_axsia(s, filename=None):
@@ -22,7 +23,7 @@ def save_axsia(s, filename=None):
 
 def axsia_to_hspy(filename, calibration_signal=None, im_shape=None):
     axsia = {}
-    
+
     axsia_in = loadmat(filename)
     if 'nrows' and 'ncols' in axsia_in.keys():
         nrows = axsia_in['nrows'][0][0]
@@ -34,8 +35,9 @@ def axsia_to_hspy(filename, calibration_signal=None, im_shape=None):
         nrows = im_shape[0]
         ncols = im_shape[1]
     else:
-        raise ValueError('SVD Decomposition requires definition of image shape')
-    
+        raise ValueError(
+            'SVD Decomposition requires definition of image shape')
+
     if 'npures' in axsia_in.keys():
         npures = axsia_in['npures'][0][0]
     else:
@@ -46,25 +48,30 @@ def axsia_to_hspy(filename, calibration_signal=None, im_shape=None):
         nchannels = axsia_in['purespectra'].shape[0]
     loadings = axsia_in['concentrations']
     factors = axsia_in['purespectra']
-    method = axsia_in['SIparams'][0][0][6][0]
-    
-    axsia['loadings'] = hs.signals.Signal2D(loadings.reshape([nrows,ncols,npures])).swap_axes(0,1).swap_axes(1,2)
-    axsia['factors'] = hs.signals.Signal1D(factors.reshape([nchannels,npures])).swap_axes(0,1)
+    # method = axsia_in['SIparams'][0][0][6][0]
+
+    axsia['loadings'] = \
+        hs.signals.Signal2D(loadings.reshape([nrows, ncols, npures]))
+    axsia['loadings'] = axsia['loadings'].swap_axes(0, 1).swap_axes(1, 2)
+
+    axsia['factors'] = \
+        hs.signals.Signal1D(factors.reshape([nchannels, npures]))
+    axsia['factors'] = axsia['factors'].swap_axes(0, 1)
+
     if calibration_signal:
         axsia['factors'].axes_manager[1].name = calibration_signal.axes_manager[-1].name
         axsia['factors'].axes_manager[1].offset = calibration_signal.axes_manager[-1].offset
         axsia['factors'].axes_manager[1].scale = calibration_signal.axes_manager[-1].scale
         axsia['factors'].axes_manager[1].units = calibration_signal.axes_manager[-1].units
-        
+
         axsia['loadings'].axes_manager[1].name = calibration_signal.axes_manager[0].name
         axsia['loadings'].axes_manager[1].offset = calibration_signal.axes_manager[0].offset
         axsia['loadings'].axes_manager[1].scale = calibration_signal.axes_manager[0].scale
         axsia['loadings'].axes_manager[1].units = calibration_signal.axes_manager[0].units
-        
+
         axsia['loadings'].axes_manager[2].name = calibration_signal.axes_manager[1].name
         axsia['loadings'].axes_manager[2].offset = calibration_signal.axes_manager[1].offset
         axsia['loadings'].axes_manager[2].scale = calibration_signal.axes_manager[1].scale
         axsia['loadings'].axes_manager[2].units = calibration_signal.axes_manager[1].units
 
     return axsia
-    
