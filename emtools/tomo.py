cbf2b64e89262bbc58bc3d7f72e3cd5da26f85f9
AndrewHerzing <aaherzing@gmail.com>
2019-07-05 15:20:41 -0400

    Added plot_points function and improved template matching

--- emtools/tomo.py
+++ emtools/tomo.py
@@ -1,17 +1,30 @@
 import numpy as np
 import cv2
+import hyperspy.api as hs
 
-def template_match(data, template, width=4, height=8):
-    thresholded = data.deepcopy()
-    thresholded.data = np.zeros([HAADF.data.shape[0],173,303],np.uint8)
+def template_match(data, template, thresh=0.8, method='cv2.TM_CCOEFF_NORMED'):
+    method = eval(method)
+    height = template.data.shape[0]
+    width = template.data.shape[1]
     points = []
+    out = data.deepcopy()
     for i in range(0,data.data.shape[0]):
-        result = cv2.matchTemplate(HAADF.data[i,:,:],template,cv2.TM_CCOEFF_NORMED)
-        result[result>0.85] = 1
-        thresholded.data[i,:,:] = np.uint8(result)
-        if (thresholded.data[i,:,:].max()>0):
-            temp = cv2.connectedComponentsWithStats(thresholded.data[i,:,:],connectivity=4)[3][1:]+[width,height]
-            for k in range(len(temp)):
-                points.append(np.append(i,temp[k][::-1]))
+        result = cv2.matchTemplate(data.data[i,:,:],template.data,method)
+        result[result>=thresh] = 1.0
+        result[result<thresh] = 0.0
+        if result.max()>0:
+            temp = cv2.connectedComponentsWithStats(np.uint8(result),connectivity=4)[3][3:]
+        for k in range(0, len(temp)):
+            points.append([i,temp[k,0]+width/2,temp[k,1]+height/2])
     points = np.array(points)
     return points
+
+def plot_points(data, points, index):
+    im = data.inav[index].deepcopy()
+    for i in np.where(points[:,0]==index):
+        print(points[i,1][0])
+        m = hs.plot.markers.point(x=points[i,1][0], y=points[i,2][0], color='red')
+        im.add_marker(m, permanent=True, plot_marker=False)
+    # im.plot()
+    # print(im.metadata.Markers)
+    return im
