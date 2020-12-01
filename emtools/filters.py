e636527d3f6cf9f87c6f55a4b8c5e16b898ae334
Herzing <aherzing@NIST.GOV>
2020-12-01 14:02:29 -0500

    Made filter functions Hyperspy compatible

--- emtools/filters.py
+++ emtools/filters.py
@@ -14,13 +14,13 @@ from scipy.signal import correlate
 import tqdm
 
 
-def butter_bpf(data, d_low, d_high, n=1):
+def butter_bpf(image, d_low, d_high, n=1):
     """
     Apply a Butterworth bandpass filter to an image or stack.
 
     Args
     ----------
-    data : Numpy array
+    image : Hyperspy Signal2D or TomoStack
         2D image or 3D stack of images
     d_low : integer
         Low-frequency cutoff of the filter
@@ -31,10 +31,11 @@ def butter_bpf(data, d_low, d_high, n=1):
 
     Returns
     ----------
-    out : Numpy array
+    out : Signal2D or TomoStack
         Filtered image or stack of images
     """
-
+    out = image.deepcopy()
+    data = image.data
     height, width = data.shape[-2:]
     [u, v] = np.meshgrid(np.arange(-np.floor(width / 2),
                          np.floor(width / 2)),
@@ -48,16 +49,15 @@ def butter_bpf(data, d_low, d_high, n=1):
     butter_hp_kernel = 1 / (1 + (0.414 * d_high / d_uv**(2 * n)))
     kernel = butter_lp_kernel * butter_hp_kernel
 
-    out = np.zeros(data.shape, data.dtype)
     if len(data.shape) == 2:
         fftshift = np.fft.fftshift(np.fft.fft2(data))
         filtered = fftshift * kernel
-        out = np.abs(np.fft.ifft2(filtered))
+        out.data = np.abs(np.fft.ifft2(filtered))
     elif len(data.shape) == 3:
         for i in tqdm.tqdm(range(0, data.shape[0])):
             fftshift = np.fft.fftshift(np.fft.fft2(data[i, :, :]))
             filtered = fftshift * kernel
-            out[i, :, :] = np.float32(np.abs(np.fft.ifft2(filtered)))
+            out.data[i, :, :] = np.float32(np.abs(np.fft.ifft2(filtered)))
     return out
 
 
@@ -84,8 +84,8 @@ def band_filter(data, in_radius=9, out_radius=60):
 
     Args
     ----------
-    data : Stack object
-        Tilt series data
+    data : Hyperspy Signal2D or TomoStack
+        Image or image stack to filter
     in_radius : integer
         Inner pixel radius for the low-frequency side of the filter
     out_radius : integer
@@ -93,21 +93,31 @@ def band_filter(data, in_radius=9, out_radius=60):
 
     Returns
     ----------
-    out : Stack object
-        Filtered copy of the inpuot stac
+    out : Hyperspy Signal2D or TomoStack
+        Filtered copy of the input data
     """
 
     out = data.deepcopy()
     im = data.data
 
-    nx, ny = im.shape[1:3]
-    a, b = (nx / 2), (ny / 2)
-    y, x = np.ogrid[-a:nx - a, -b:nx - b]
-    mask = np.logical_xor(x**2 + y**2 <= out_radius**2,
-                          x**2 + y**2 < in_radius**2)
-
-    im_freq = np.fft.fft2(im)
-    im_filt = np.fft.fftshift(im_freq) * mask
-    out.data = np.float32(np.real(np.fft.ifft2(np.fft.ifftshift(im_filt))))
-
+    if len(im.shape) == 2:
+        nx, ny = im.shape
+        a, b = (nx / 2), (ny / 2)
+        y, x = np.ogrid[-a:nx - a, -b:nx - b]
+        mask = np.logical_xor(x**2 + y**2 <= out_radius**2,
+                              x**2 + y**2 < in_radius**2)
+        im_freq = np.fft.fft2(im)
+        im_filt = np.fft.fftshift(im_freq) * mask
+        out.data = np.float32(np.real(np.fft.ifft2(np.fft.ifftshift(im_filt))))
+    elif len(im.shape) == 3:
+        nx, ny = im.shape[1:3]
+        a, b = (nx / 2), (ny / 2)
+        y, x = np.ogrid[-a:nx - a, -b:nx - b]
+        mask = np.logical_xor(x**2 + y**2 <= out_radius**2,
+                              x**2 + y**2 < in_radius**2)
+        for i in tqdm.tqdm(range(0, data.shape[0])):
+            im_freq = np.fft.fft2(im[i, :, :])
+            im_filt = np.fft.fftshift(im_freq) * mask
+            out.data[i, :, :] =\
+                np.float32(np.real(np.fft.ifft2(np.fft.ifftshift(im_filt))))
     return out
