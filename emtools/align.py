fcc543c1ae98d432efc5cc951031dd1710f83a1b
Herzing <aherzing@NIST.GOV>
2020-12-04 15:05:14 -0500

    Fixed flake8 errors

--- emtools/align.py
+++ emtools/align.py
@@ -1,15 +1,15 @@
 import numpy as np
-from scipy import ndimage
-import tqdm
-import copy
 import hyperspy.api as hs
 from pystackreg import StackReg
+
 
 def get_stackreg_shifts(stack):
     sr = StackReg(StackReg.TRANSLATION)
     sr_shifts = sr.register_stack(stack.data)
-    sr_shifts = np.array([sr_shifts[i][:-1,2][::-1] for i in range(0, len(sr_shifts))])
+    sr_shifts = np.array([sr_shifts[i][:-1, 2][::-1]
+                          for i in range(0, len(sr_shifts))])
     return sr_shifts
+
 
 def apply_hanning(image):
     h = np.hanning(image.data.shape[0])
@@ -17,12 +17,15 @@ def apply_hanning(image):
     image.data = image.data * ham2d
     return image
 
+
 def apply_taper(image, taper_percent):
     width = np.int32(np.round(taper_percent / 100 * image.data.shape[0]))
     image.data = np.pad(image.data, pad_width=width, mode='linear_ramp')
     return image
 
-def get_ps(s, crop=True, hanning=False, taper=False, taper_percent=3, crop_factor=4):
+
+def get_ps(s, crop=True, hanning=False, taper=False, taper_percent=3,
+           crop_factor=4):
     image = s.deepcopy()
     if hanning:
         image = apply_hanning(image)
