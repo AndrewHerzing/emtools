709931b354d1a1ee8822e3cf60bed1a2ef6b5553
Herzing <aherzing@NIST.GOV>
2020-12-04 15:16:37 -0500

    Added docstrings to align.py

--- emtools/align.py
+++ emtools/align.py
@@ -4,6 +4,20 @@ from pystackreg import StackReg
 
 
 def get_stackreg_shifts(stack):
+    """
+    Calculate alignment shifts for image stack using PyStackReg.
+
+    Args
+    ----------
+    stack : Hyperspy Signal2D
+        Image stack to align.
+
+    Returns
+    ----------
+    sr_shifts : NumPy array
+        Calculated alignment shifts.
+
+    """
     sr = StackReg(StackReg.TRANSLATION)
     sr_shifts = sr.register_stack(stack.data)
     sr_shifts = np.array([sr_shifts[i][:-1, 2][::-1]
@@ -12,6 +26,20 @@ def get_stackreg_shifts(stack):
 
 
 def apply_hanning(image):
+    """
+    Apply a Hanning window to an image to remove artifacts in FFT.
+
+    Args
+    ----------
+    image : Hyperspy Signal2D
+        Image to taper.
+
+    Returns
+    ----------
+    image : Hyperspy Signal2D
+        Image after applying the Hanning window.
+
+    """
     h = np.hanning(image.data.shape[0])
     ham2d = np.sqrt(np.outer(h, h))
     image.data = image.data * ham2d
@@ -19,6 +47,23 @@ def apply_hanning(image):
 
 
 def apply_taper(image, taper_percent):
+    """
+    Taper image edge pixels to remove artifacts in FFT.
+
+    Args
+    ----------
+    image : Hyperspy Signal2D
+        Image to taper.
+    taper_percent : int
+        Percent of image pixels to taper at the edge.
+
+    Returns
+    ----------
+    image : Hyperspy Signal2D
+        Image with tapered edge pixels.
+
+    """
+
     width = np.int32(np.round(taper_percent / 100 * image.data.shape[0]))
     image.data = np.pad(image.data, pad_width=width, mode='linear_ramp')
     return image
@@ -26,6 +71,35 @@ def apply_taper(image, taper_percent):
 
 def get_ps(s, crop=True, hanning=False, taper=False, taper_percent=3,
            crop_factor=4):
+    """
+    Return the Fourier power spectrum of an image.
+
+    Args
+    ----------
+    s : Hyperspy Signal2D
+        Image for which to calculate the power spectrum
+    crop : bool
+        If true, crop the power spectrum output to a factor set by crop_factor.
+        Default is True.
+    hannign : bool
+        If true, apply a Hannign window to the image prior to calculating FFT.
+        Default is True.
+    taper : bool
+        If True, taper the edge pixels of the image prior to calculating FFT.
+        Default is False.
+    taper_percent : int
+        Percent of image pixels to taper at the edge. Default is 3.
+    crop_factor : int
+        Factor to crop the output power spectrum by. Default is 4.
+
+
+    Returns
+    ----------
+    ps : Hyperspy Signal2D
+        Power spectrum of the input image
+
+    """
+
     image = s.deepcopy()
     if hanning:
         image = apply_hanning(image)
